9c6938ed66367342fa54ac9c745f52027ec1c30f
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-05 15:28:32 +0300

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,75 @@
-import React, { useState, useRef, useImperativeHandle } from "react";
+import React, { useReducer } from "react";
+import "./index.css";
+
+const limitRGB = (num) => (num < 0 ? 0 : num > 255 ? 255 : num);
+const step = 50;
+
+const reducer = (state, { type, payload = step }) => {
+  switch (type) {
+    case "INCREMENT_R":
+      return {
+        ...state,
+        r: limitRGB(state.r + payload),
+      };
+    case "DECREMENT_R":
+      return {
+        ...state,
+        r: limitRGB(state.r - payload),
+      };
+    case "INCREMENT_G":
+      return {
+        ...state,
+        g: limitRGB(state.g + payload),
+      };
+    case "DECREMENT_G":
+      return {
+        ...state,
+        g: limitRGB(state.g - payload),
+      };
+    case "INCREMENT_B":
+      return {
+        ...state,
+        b: limitRGB(state.b + payload),
+      };
+    case "DECREMENT_B":
+      return {
+        ...state,
+        b: limitRGB(state.b - payload),
+      };
+    default:
+      return state;
+  }
+};
 
 export default function App() {
+  const [{ r, g, b }, dispatch] = useReducer(reducer, { r: 0, g: 150, b: 200 });
+
   return (
     <div className="App">
-      <Form />
+      <h1
+        style={{
+          color: `rgb(${r}, ${g}, ${b})`,
+        }}
+      >
+        Hello CodeSandbox
+      </h1>
+      <div>
+        <span>r </span>
+        <button onClick={() => dispatch({ type: "INCREMENT_R", payload: 100 })}>
+          +
+        </button>
+        <button onClick={() => dispatch({ type: "DECREMENT_R" })}>-</button>
+      </div>
+      <div>
+        <span>g </span>
+        <button onClick={() => dispatch({ type: "INCREMENT_G" })}>+</button>
+        <button onClick={() => dispatch({ type: "DECREMENT_G" })}>-</button>
+      </div>
+      <div>
+        <span>b </span>
+        <button onClick={() => dispatch({ type: "INCREMENT_B" })}>+</button>
+        <button onClick={() => dispatch({ type: "DECREMENT_B" })}>-</button>
+      </div>
     </div>
   );
 }
-
-const TextInput = React.forwardRef((props, ref) => {
-  const { hasError, placeholder, value, update } = props;
-  console.log(update);
-  const inputRef = useRef();
-
-  useImperativeHandle(ref, () => {
-    return {
-      focus() {
-        inputRef.current.focus();
-      },
-    };
-  });
-
-  return (
-    <input
-      ref={inputRef}
-      value={value}
-      onChange={(e) => update(e.target.value)}
-      placeholder={placeholder}
-      style={{
-        borderColor: hasError ? "red" : "black",
-      }}
-    />
-  );
-});
-
-const Form = () => {
-  const [card, setCard] = useState("");
-  const [phone, setPhone] = useState("");
-  const [error, setError] = useState("");
-
-  const cardEl = useRef(); // current = {focus() {}}
-  const phoneEl = useRef();
-
-  const validate = () => {
-    if (card.length < 16) {
-      setError("card");
-      cardEl.current.focus(); // метод пришел снизу
-      return;
-    }
-
-    if (phone.length < 11) {
-      setError("phone");
-      phoneEl.current.focus();
-      return;
-    }
-    setError("");
-  };
-
-  return (
-    <div>
-      <h2>useImperativeHandle hook</h2>
-      <TextInput
-        hasError={error === "card"}
-        placeholder={"Card"}
-        ref={cardEl}
-        value={card}
-        update={setCard}
-      />
-      <TextInput
-        hasError={error === "phone"}
-        placeholder={"Phone"}
-        value={phone}
-        update={setPhone}
-        ref={phoneEl}
-      />
-      <button onClick={validate}>Validate</button>
-    </div>
-  );
-};
