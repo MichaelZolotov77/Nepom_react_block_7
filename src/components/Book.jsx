b18aee74d6f47e3e82afe86e5a7cd2e93e74d654
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-05 12:59:09 +0300

    useCallback, useMemo начальный код

--- src/components/Book.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useContext } from "react";
-import { CustomContext } from "../hooks/Context";
-
-export function Book(props) {
-  const { removeBook } = useContext(CustomContext);
-  return <h2 onClick={() => removeBook(props.id)}>{props.title}</h2>;
-}
